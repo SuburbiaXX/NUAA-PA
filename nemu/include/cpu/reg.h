bcd3042bb193d1dd32bca94d77a59a0dc685b7a4
tracer-ics2017 <tracer@njuics.org>
2022-03-16 22:23:51 +0800

    >  compile 162020228 chentailun Linux debian 4.19.0-18-686 #1 SMP Debian 4.19.208-1 (2021-09-29) i686 GNU/Linux  22:23:51 up 10 min,  1 user,  load average: 0.00, 0.00, 0.00 9e3dd8239d96bdf54c75773f94b6abd24756817f

--- nemu/include/cpu/reg.h
+++ nemu/include/cpu/reg.h
@@ -16,7 +16,7 @@ enum { R_AL, R_CL, R_DL, R_BL, R_AH, R_CH, R_DH, R_BH };
 
 typedef struct {
   // struct {
-  union{
+  //union{
     union{
       uint32_t _32;
       uint16_t _16;
@@ -25,7 +25,7 @@ typedef struct {
     struct{
        rtlreg_t eax, ecx, edx, ebx, esp, ebp, esi, edi;
     };
-  };
+  //};
   /* Do NOT change the order of the GPRs' definitions. */
 
   /* In NEMU, rtlreg_t is exactly uint32_t. This makes RTL instructions
