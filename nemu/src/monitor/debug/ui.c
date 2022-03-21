48644ff578c67b12e9909247956afaf2fd53e341
tracer-ics2017 <tracer@njuics.org>
2022-03-21 20:01:49 +0800

    >  compile 162020228 chentailun Linux debian 4.19.0-18-686 #1 SMP Debian 4.19.208-1 (2021-09-29) i686 GNU/Linux  20:01:49 up 47 min,  1 user,  load average: 0.00, 0.00, 0.00 b7219133c94f483e4259ffe5fe6e699b77c8d1d6

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -117,6 +117,20 @@ static int cmd_info(char *args){
 }
 
 static int cmd_x(char *args){
+	char *time=strtok(NULL," ");
+	if (time==NULL){
+		printf("Lack of two parameters!!!");
+		return 0;
+	}
+	char *start=strtok(NULL," ");
+	if(start==NULL){
+		printf("Lack of one parameter!!!");
+		return 0;
+	}
+	int nums=atoi(time);
+
+	printf("test\n");
+	printf("%d\t0x%s\n",nums,start);
 
 	return 0;
 }
