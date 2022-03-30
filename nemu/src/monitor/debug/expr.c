f3f05da662929d529e4115c6f34a676bcde94150
tracer-ics2017 <tracer@njuics.org>
2022-03-30 21:24:13 +0800

    >  compile 162020228 chentailun Linux debian 4.19.0-18-686 #1 SMP Debian 4.19.208-1 (2021-09-29) i686 GNU/Linux  21:24:13 up  2:36,  1 user,  load average: 0.00, 0.00, 0.00 caa887f136878b1230302c1cef88c90058c859a

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -221,6 +221,7 @@ uint32_t eval(int p, int q) {
 			op =find_dominated_op(p,q);
       val1 = eval(p, op - 1);
       val2 = eval(op + 1, q);
+			printf("val1 = %d  val2 = %d\n",val1,val2);
       switch(tokens[op].type){
         case '+': return val1 + val2;
         case '-': return val1 - val2;
