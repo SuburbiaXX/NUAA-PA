c7cb3d8880dd4aaeb180a3abb5159cae492aa0e4
tracer-ics2017 <tracer@njuics.org>
2022-03-31 10:56:06 +0800

    >  compile 162020228 chentailun Linux debian 4.19.0-18-686 #1 SMP Debian 4.19.208-1 (2021-09-29) i686 GNU/Linux  10:56:06 up 30 min,  1 user,  load average: 0.00, 0.00, 0.00 d62314ce8fdac1e8c807c522ec4ff530b92ed096

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -254,7 +254,7 @@ uint32_t expr(char *e, bool *success) {
   /* TODO: Insert codes to evaluate the expression. */
   //TODO();
 	for (int i=0;i<nr_token;i++){
-		if(tokens[i].type=='-' && (i==0 || tokens[i-1].type=='(' || tokens[i-1].type=='+' || tokens[i-1].type=='-' || tokens[i-1].type=='*' || tokens[i-1].type=='/' )){
+		if(tokens[i].type=='-' && ((i==0 || tokens[i-1].type=='(' || tokens[i-1].type=='+' || tokens[i-1].type=='-' || tokens[i-1].type=='*' || tokens[i-1].type=='/') || (tokens[i-1].type==TK_NEG && (tokens[i+1].type==TK_DEC || tokens[i+1].type==TK_HEX)))){
 			tokens[i].type=TK_NEG;
 			printf("The minus position is %d\n",i);
 		}
