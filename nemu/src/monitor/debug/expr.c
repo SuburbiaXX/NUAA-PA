53994e5128fb58b3c52cda9bcd0e0ad66ea3a61c
tracer-ics2017 <tracer@njuics.org>
2022-03-31 21:05:23 +0800

    >  compile 162020228 chentailun Linux debian 4.19.0-18-686 #1 SMP Debian 4.19.208-1 (2021-09-29) i686 GNU/Linux  21:05:23 up 38 min,  1 user,  load average: 0.05, 0.02, 0.00 61c4058aa04c66322582637f225460b7d1ad91d8

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -177,19 +177,19 @@ int find_dominated_op(int p, int q){
 			}
 		}*/
 			if(tokens[i].type==TK_NEG){
-				if(level<=0){
+				if(level<0){
 					level=0;
 					pos=i;
 				}
 			}
 			if(tokens[i].type=='*' || tokens[i].type=='/'){
-				if(level<=1){
+				if(level<1){
 					level=1;
 					pos=i;
 				}
 			}
 			if(tokens[i].type=='+' || tokens[i].type=='-'){
-				if(level<=2){
+				if(level<2){
 					level=2;
 					pos=i;
 				}
