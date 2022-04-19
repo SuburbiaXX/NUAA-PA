f2cdf4301ab709a4dbd79b778029b5ee72d7f844
tracer-ics2017 <tracer@njuics.org>
2022-04-19 21:04:37 +0800

    >  compile 162020228 chentailun Linux debian 4.19.0-18-686 #1 SMP Debian 4.19.208-1 (2021-09-29) i686 GNU/Linux  21:04:37 up 10:33,  1 user,  load average: 0.00, 0.00, 0.00 d21d8a74c91f49a94838cb4abc514fe66121fcb1

--- nemu/src/cpu/exec/arith.c
+++ nemu/src/cpu/exec/arith.c
@@ -7,8 +7,18 @@ make_EHelper(add) {
 }
 
 make_EHelper(sub) {
-  TODO();
-
+  rtlreg_t temp1,temp2,temp3;
+	rtl_sub(&temp1,&id_dest->val,&id_src->val);
+	operand_write(id_dest,&temp1);
+	//update eflags status
+	rtl_update_ZFSF(&temp1,id_dest->width);
+	rtl_sltu(&temp2,&id_dest->val,&temp1);
+	rtl_set_CF(&temp2);
+	rtl_xor(&temp2,&id_dest->val,&id_src->val);
+	rtl_xor(&temp3,&id_dest->val,&temp1);
+	rtl_and(&temp2,&temp2,&temp3);
+	rtl_msb(&temp2,&temp2,id_dest->width);
+	rtl_set_OF(&temp2);
   print_asm_template2(sub);
 }
 
