cbffb57348be47a48975cc7654646a1bfc201bd5
tracer-ics2017 <tracer@njuics.org>
2022-04-26 20:45:09 +0800

    >  compile 162020228 chentailun Linux debian 4.19.0-18-686 #1 SMP Debian 4.19.208-1 (2021-09-29) i686 GNU/Linux  20:45:09 up  4:27,  1 user,  load average: 0.00, 0.00, 0.00 eacfce05dd61f48b486e93334c556dc594bd4b46

--- nemu/src/cpu/exec/cc.c
+++ nemu/src/cpu/exec/cc.c
@@ -13,15 +13,38 @@ void rtl_setcc(rtlreg_t* dest, uint8_t subcode) {
 
   // TODO: Query EFLAGS to determine whether the condition code is satisfied.
   // dest <- ( cc is satisfied ? 1 : 0)
+	
+	rtlreg_t temp1,temp2,temp3;
   switch (subcode & 0xe) {
     case CC_O:
+			rtl_get_OF(dest);
+			break;
     case CC_B:
+			rtl_get_CF(dest);
+			break;
     case CC_E:
+			rtl_get_ZF(dest);
+			break;
     case CC_BE:
+			rtl_get_ZF(&temp1);
+			rtl_get_CF(&temp2);
+			rtl_or(dest,&temp1,&temp2);
+			break;
     case CC_S:
+			rtl_get_SF(dest);
+			break;
     case CC_L:
+			rtl_get_SF(&temp1);
+			rtl_get_OF(&temp2);
+			rtl_xor(dest,&temp1,&temp2);
+			break;
     case CC_LE:
-      TODO();
+			rtl_get_ZF(&temp1);
+			rtl_get_SF(&temp2);
+			rtl_get_OF(&temp3);
+			rtl_xor(&temp2,&temp2,&temp3);
+			rtl_or(dest,&temp1,&temp2);
+			break;
     default: panic("should not reach here");
     case CC_P: panic("n86 does not have PF");
   }
