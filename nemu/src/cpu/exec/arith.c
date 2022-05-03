7610b07ae9194210ec849afcd8cb2abf68a9f81e
tracer-ics2017 <tracer@njuics.org>
2022-05-03 19:28:26 +0800

    >  compile 162020228 chentailun Linux debian 4.19.0-18-686 #1 SMP Debian 4.19.208-1 (2021-09-29) i686 GNU/Linux  19:28:26 up  1:34,  1 user,  load average: 0.12, 0.35, 0.23 906dc590d77109bc4df7a51c21f74345bd6309c4

--- nemu/src/cpu/exec/arith.c
+++ nemu/src/cpu/exec/arith.c
@@ -1,7 +1,7 @@
 #include "cpu/exec.h"
 
 make_EHelper(add) {
-/*  rtlreg_t temp1,temp2;
+  rtlreg_t temp1,temp2;
 	rtl_add(&temp1,&id_dest->val,&id_src->val);
 	operand_write(id_dest,&temp1);
 	rtl_update_ZFSF(&temp1,id_dest->width);
@@ -14,29 +14,10 @@ make_EHelper(add) {
 	rtl_msb(&temp1,&temp1,id_dest->width);
 	rtl_set_OF(&temp1);
   print_asm_template2(add);
-*/
-		rtlreg_t k;
-    rtl_add(&t2, &id_dest->val, &id_src->val);
-    rtl_sltu(&k, &t2, &id_dest->val);
-    operand_write(id_dest, &t2);
- 
-    rtl_update_ZFSF(&t2, id_dest->width);
- 
-    rtl_sltu(&t0, &t2, &id_dest->val);
-    rtl_or(&t0, &k, &t0);
-    rtl_set_CF(&t0);
- 
-    rtl_xor(&t0, &id_dest->val, &id_src->val);
-    rtl_not(&t0);
-    rtl_xor(&t1, &id_dest->val, &t2);
-    rtl_and(&t0, &t0, &t1);
-    rtl_msb(&t0, &t0, id_dest->width);
-    rtl_set_OF(&t0);
-    print_asm_template2(add);
 }
 
 make_EHelper(sub) {
-  /*rtlreg_t temp1,temp2,temp3;
+  rtlreg_t temp1,temp2,temp3;
 	rtl_sub(&temp1,&id_dest->val,&id_src->val);
 	operand_write(id_dest,&temp1);
 	//update eflags status
@@ -49,24 +30,11 @@ make_EHelper(sub) {
 	rtl_msb(&temp2,&temp2,id_dest->width);
 	rtl_set_OF(&temp2);
   print_asm_template2(sub);
-*/
-		rtlreg_t k;
-    rtl_sub(&t2, &id_dest->val, &id_src->val);
-		rtl_sltu(&k, &id_dest->val, &id_src->val);
-    operand_write(id_dest, &t2);
- 
-    rtl_update_ZFSF(&t2, id_dest->width);
-    rtl_set_CF(&k);
-    rtl_xor(&t0, &id_dest->val, &id_src->val);
-    rtl_xor(&t1, &id_dest->val, &t2);
-    rtl_and(&t0, &t0, &t1);
-    rtl_msb(&t0, &t0, id_dest->width);
-    rtl_set_OF(&t0);
-  print_asm_template2(sub);
+
 }
 
 make_EHelper(cmp) {
-  rtlreg_t temp1,temp2;
+ /* rtlreg_t temp1,temp2;
 	rtl_sub(&temp1,&id_dest->val,&id_src->val);
 	rtl_update_ZFSF(&temp1,id_dest->width);
 	rtl_sltu(&temp2,&id_dest->val,&temp1);
@@ -77,10 +45,29 @@ make_EHelper(cmp) {
 	rtl_msb(&temp1,&temp1,id_dest->width);
 	rtl_set_OF(&temp1);
   print_asm_template2(cmp);
+*/
+	rtlreg_t k,o;
+	//printf("eax=%#x ",cpu.eax);
+    rtl_sub(&t2, &id_dest->val, &id_src->val);
+    rtl_sltu(&k, &id_dest->val, &t2);
+	//printf("eax'=%#x\n",cpu.eax); 
+    rtl_update_ZFSF(&t2, id_dest->width);
+ 
+    rtl_sltu(&o, &id_dest->val, &t2);
+    rtl_or(&o, &k, &o);
+    rtl_set_CF(&o);
+    rtl_xor(&o, &id_dest->val, &id_src->val);
+	//printf("eax'=%#x\n",cpu.eax);
+    rtl_xor(&t1, &id_dest->val, &t2);
+    rtl_and(&o, &o, &t1);
+    rtl_msb(&o, &o, id_dest->width);
+    rtl_set_OF(&o);
+	//printf("eax'=%#x\n",cpu.eax);
+  print_asm_template2(cmp);
 }
 
 make_EHelper(inc) {
-	rtlreg_t temp1=id_dest->val,temp2;
+	/*rtlreg_t temp1=id_dest->val,temp2;
 	temp1++;
 	rtl_xor(&temp2,&temp1,&id_dest->val);
 	rtl_msb(&temp2,&temp2,id_dest->width);
@@ -88,10 +75,18 @@ make_EHelper(inc) {
 	operand_write(id_dest,&temp1);
 	rtl_update_ZFSF(&temp1,id_dest->width);
   print_asm_template1(inc);
+*/
+rtl_addi(&t0,&id_dest->val,1);
+  operand_write(id_dest,&t0);
+  rtl_xor(&t1,&id_dest->val,&t0);
+  rtl_msb(&t1,&t1, id_dest->width);
+  rtl_set_OF(&t1);
+  rtl_update_ZFSF(&t0, id_dest->width);
+  print_asm_template1(inc);
 }
 
 make_EHelper(dec) {
-  rtlreg_t temp1,temp2,temp3=1;
+  /*rtlreg_t temp1,temp2,temp3=1;
 	rtl_subi(&temp1,&id_dest->val,1);
 	rtl_update_ZFSF(&temp1,id_dest->width);
 	rtl_xor(&temp2,&id_dest->val,&temp3);
@@ -100,6 +95,15 @@ make_EHelper(dec) {
 	rtl_msb(&temp3,&temp3,id_dest->width);
 	rtl_set_OF(&temp3);
 	operand_write(id_dest,&temp1);
+  print_asm_template1(dec);
+*/
+rtl_subi(&t0,&id_dest->val,1);
+  operand_write(id_dest,&t0);
+  rtlreg_t i;
+  rtl_xor(&i,&id_dest->val,&t0);
+  rtl_msb(&i,&i,id_dest->width);
+  rtl_set_OF(&i);
+  rtl_update_ZFSF(&t0, id_dest->width);
   print_asm_template1(dec);
 }
 
