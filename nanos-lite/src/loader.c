7ba59c7921ea47b2b5ffc53bff8ed0e22a860bab
tracer-ics2017 <tracer@njuics.org>
2022-05-15 23:11:44 +0800

    >  compile 162020228 chentailun Linux debian 4.19.0-18-686 #1 SMP Debian 4.19.208-1 (2021-09-29) i686 GNU/Linux  23:11:44 up 24 min,  1 user,  load average: 0.24, 0.09, 0.08 827c049fb2f6dc14a21daf2332bc1a5e7d8e241d

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -1,8 +1,10 @@
 #include "common.h"
-
+//#include "fs.c"
+extern size_t get_ramdisk_size();
+extern void ramdisk_read(void *buf,off_t offset,size_t len);
 #define DEFAULT_ENTRY ((void *)0x4000000)
 
 uintptr_t loader(_Protect *as, const char *filename) {
-  TODO();
+	ramdisk_read(DEFAULT_ENTRY,0,get_ramdisk_size());
   return (uintptr_t)DEFAULT_ENTRY;
 }
