0659214e16307de12120ee162e860f9ce57f3bf0
tracer-ics2017 <tracer@njuics.org>
2022-05-29 22:41:48 +0800

    >  compile 162020228 chentailun Linux debian 4.19.0-18-686 #1 SMP Debian 4.19.208-1 (2021-09-29) i686 GNU/Linux  22:41:48 up  1:10,  1 user,  load average: 0.29, 0.09, 0.02 14cacecbae94d3a9d5fcba4431c8e8977f3d87fc

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -1,9 +1,16 @@
 #include "common.h"
+#include "fs.h"
 extern size_t get_ramdisk_size();
 extern void ramdisk_read(void *buf,off_t offset,size_t len);
 #define DEFAULT_ENTRY ((void *)0x4000000)
 
 uintptr_t loader(_Protect *as, const char *filename) {
-	ramdisk_read(DEFAULT_ENTRY,0,get_ramdisk_size());
-  return (uintptr_t)DEFAULT_ENTRY;
+	//ramdisk_read(DEFAULT_ENTRY,0,get_ramdisk_size());
+	int file;
+	size_t len;
+	file=fs_open(filename,0,0);
+	len=fs_filesz(file);
+	fs_read(file,DEFAULT_ENTRY,len);
+	fs_close(file);
+ 	return (uintptr_t)DEFAULT_ENTRY;
 }
--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -5,18 +5,16 @@
 int mm_brk(uint32_t new_brk);
 
 static inline uintptr_t sys_open(uintptr_t pathname, uintptr_t flags, uintptr_t mode) {
-  TODO();
-  return 1;
+  return fs_open((char *)pathname,flags,mode);
 }
 
 static inline uintptr_t sys_write(uintptr_t fd, uintptr_t buf, uintptr_t len) {
-  Log();
+  //Log();
 	return fs_write(fd,(void *)buf,len);
 }
 
 static inline uintptr_t sys_read(uintptr_t fd, uintptr_t buf, uintptr_t len) {
-  TODO();
-  return 1;
+  return fs_read(fd,(void *)buf,len);
 }
 
 static inline uintptr_t sys_lseek(uintptr_t fd, uintptr_t offset, uintptr_t whence) {
@@ -24,8 +22,7 @@ static inline uintptr_t sys_lseek(uintptr_t fd, uintptr_t offset, uintptr_t when
 }
 
 static inline uintptr_t sys_close(uintptr_t fd) {
-  TODO();
-  return 1;
+  return fs_close(fd);
 }
 
 static inline uintptr_t sys_brk(uintptr_t new_brk) {
@@ -61,6 +58,18 @@ _RegSet* do_syscall(_RegSet *r) {
 			break;
 		case SYS_brk:
 			SYSCALL_ARG1(r)=0;
+			break;
+		case SYS_open:
+			SYSCALL_ARG1(r)=sys_open(a[1],a[2],a[3]);
+			break;
+		case SYS_read:
+			SYSCALL_ARG1(r)=sys_read(a[1],a[2],a[3]);
+			break;
+		case SYS_lseek:
+			SYSCALL_ARG1(r)=sys_lseek(a[1],a[2],a[3]);
+			break;
+		case SYS_close:
+			SYSCALL_ARG1(r)=sys_close(a[1]);
 			break;
 		default: panic("Unhandled syscall ID = %d", a[0]);
   }
