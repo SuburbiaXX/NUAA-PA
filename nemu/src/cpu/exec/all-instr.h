a02d24ab0144f6f67f273952f04733c2664eb0f9
tracer-ics2017 <tracer@njuics.org>
2022-05-29 17:08:50 +0800

    >  compile 162020228 chentailun Linux debian 4.19.0-18-686 #1 SMP Debian 4.19.208-1 (2021-09-29) i686 GNU/Linux  17:08:50 up  1:38,  1 user,  load average: 0.06, 0.03, 0.01 27a22d08f2764a9dc9f1b2429c957377b66134

--- nemu/src/cpu/exec/all-instr.h
+++ nemu/src/cpu/exec/all-instr.h
@@ -51,3 +51,5 @@ make_EHelper(rol);
 make_EHelper(lidt);
 make_EHelper(int);
 make_EHelper(pusha);
+make_EHelper(popa);
+make_EHelper(iret);
--- nemu/src/cpu/exec/data-mov.c
+++ nemu/src/cpu/exec/data-mov.c
@@ -32,7 +32,16 @@ make_EHelper(pusha) {
 }
 
 make_EHelper(popa) {
-  TODO();
+  rtlreg_t temp;
+	rtl_pop(&cpu.edi);
+	rtl_pop(&cpu.esi);
+	rtl_pop(&cpu.ebp);
+	rtl_pop(&temp);
+	rtl_pop(&cpu.ebx);
+	rtl_pop(&cpu.edx);
+	rtl_pop(&cpu.ecx);
+	rtl_pop(&cpu.eax);
+
 
   print_asm("popa");
 }
