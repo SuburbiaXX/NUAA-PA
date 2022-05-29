6698f99a53f05b9ede113ae2c28fba22b4a4d749
tracer-ics2017 <tracer@njuics.org>
2022-05-29 21:28:34 +0800

    >  run 162020228 chentailun Linux debian 4.19.0-18-686 #1 SMP Debian 4.19.208-1 (2021-09-29) i686 GNU/Linux  21:28:34 up 15 min,  1 user,  load average: 0.00, 0.00, 0.00 654815025055d069b66913299edac5e36f1b1e5

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -29,7 +29,6 @@ static inline uintptr_t sys_close(uintptr_t fd) {
 }
 
 static inline uintptr_t sys_brk(uintptr_t new_brk) {
-  //TODO();
   return 0;
 }
 
@@ -61,7 +60,8 @@ _RegSet* do_syscall(_RegSet *r) {
 			SYSCALL_ARG1(r)=sys_write(a[1],a[2],a[3]);
 			break;
 		case SYS_brk:
-			SYSCALL_ARG1(r)=0;
+			SYSCALL_ARG1(r)=sys_brk(a[1]);	
+		//SYSCALL_ARG1(r)=0;
 			break;
 		default: panic("Unhandled syscall ID = %d", a[0]);
   }
